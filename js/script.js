823351a06e3b4bfa0f7d6bad769f959f0f6a227a
adrian44s <94121736+adrian44s@users.noreply.github.com>
2023-04-21 21:03:15 -0500

    changed index and script.js
    added input in the index and added a function that check if you typed was done incorrectly or correctly

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
-const typingTest= document.querySelector(".typing-test p");
+const typingTest = document.querySelector(".typing-test p");
+const inputField = document.querySelector(".wrapper .input");
+let charInd = 0;
 
 function pickRamdomQuote() {
-    let randomIn = Math.floor(Math.random()*quotes.length)
-    quotes[randomIn].split("").forEach(span =>{
-        let stag = `<span>${span}</span>`;
-        typingTest.innerHTML += stag;
-    });
+  let randomIn = Math.floor(Math.random() * quotes.length);
+  quotes[randomIn].split("").forEach((span) => {
+    let stag = `<span>${span}</span>`;
+    typingTest.innerHTML += stag;
+  });
+
+  document.addEventListener("keydown", () => inputField.focus());
+  typingTest.addEventListener("click", () => inputField.focus());
+}
+
+function initTyping() {
+  const charac = typingTest.querySelectorAll("span");
+  let typedChar = inputField.value.split('')[charInd];
+  if(charac[charInd].innerText === typedChar){
+    charac[charInd].classList.add('Correct')
+  }else{
+    charac[charInd].classList.add('Incorrect')
+  }
+  charInd++
 }
 
 pickRamdomQuote();
+inputField.addEventListener("input", initTyping);
